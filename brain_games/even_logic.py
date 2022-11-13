22b333a26658f80c201f9b6cd258590fc22e7dc5
HoldCarter <holdcarter@holdcarter.ru>
2022-11-13 18:57:29 +0300

    linter_edits

--- brain_games/even_logic.py
+++ brain_games/even_logic.py
@@ -5,11 +5,11 @@ from cli import welcome_user
 def even():
     print("Welcome to the Brain Games!")
     name = welcome_user()
-    print(f"Answer 'yes' if number is even, otherwise answer 'no'")
+    print("Answer 'yes' if number is even, otherwise answer 'no'")
     count_correct_answers = 0
     while count_correct_answers < 3:
         res = correct_answer()
-        if res == True:
+        if res:
             count_correct_answers += 1
             print("Correct!")
             if count_correct_answers == 3:
@@ -23,11 +23,12 @@ def correct_answer():
     number = randint(1, 100)
     print(f"Question:{number}")
     answer = input().lower()
-    if (answer == 'yes' and number % 2 == 0) or (answer == 'no' and number % 2 != 0):
+    if ((answer == 'yes' and number % 2 == 0) or
+            (answer == 'no' and number % 2 != 0)):
         return True
     elif number % 2 == 0:
-        return (f"'{answer}' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, name!")
+        return (f"'{answer}' is wrong answer ;(. Correct answer was 'yes'.\
+                \nLet's try again, name!")
     elif number % 2 != 0:
-        return (f"'{answer}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, name!")
-
-
+        return (f"'{answer}' is wrong answer ;(. Correct answer was 'no'.\
+                \nLet's try again, name!")
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,11 +1,9 @@
+from even_logic import even
 
-from even_logic import even, correct_answer
 
 def main():
     even()
 
 
-
 if __name__ == '__main__':
     main()
-
