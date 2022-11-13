a11912fbd18d687c63ae52019d3604f47376e0db
HoldCarter <holdcarter@holdcarter.ru>
2022-11-14 00:21:29 +0300

    add asciinema

--- brain_games/even_logic.py
+++ brain_games/even_logic.py
@@ -9,7 +9,7 @@ def even():
     print("Answer 'yes' if number is even, otherwise answer 'no'")
     count_correct_answers = 0
     while count_correct_answers < 3:
-        res = correct_answer()
+        res = correct_answer(name)
         if res is True:
             count_correct_answers += 1
             print("Correct!")
@@ -20,16 +20,16 @@ def even():
             break
 
 
-def correct_answer():
+def correct_answer(name):
     number = randint(1, 100)
     print(f"Question:{number}")
-    answer = input().lower()
+    answer = input("Your answer: ").lower()
     if ((answer == 'yes' and number % 2 == 0) or
             (answer == 'no' and number % 2 != 0)):
         return True
     elif number % 2 == 0:
         return (f"'{answer}' is wrong answer ;(. Correct answer was 'yes'.\
-                \nLet's try again, name!")
+                \nLet's try again, {name}!")
     elif number % 2 != 0:
         return (f"'{answer}' is wrong answer ;(. Correct answer was 'no'.\
-                \nLet's try again, name!")
+                \nLet's try again, {name}!")
